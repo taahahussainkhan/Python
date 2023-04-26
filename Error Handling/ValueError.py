76ff754688d0220ccd76886b3ed385ab6f9b4332
taahahussainkhan <taaha128@gmail.com>
2023-04-27 00:43:42 +0500

    Dataframe operations

---
+++ Error Handling/ValueError.py
@@ -0,0 +1,10 @@
+while True:
+    try:
+        x=int(input("Enter x: "))
+    except ValueError:
+        print(f'x is not and integar!')
+    else:
+        break
+print(f'x is {x}')
+    
+    #This will prompt the user again and again untill the correct input is entered.
